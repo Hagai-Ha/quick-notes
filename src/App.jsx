024815a67f68e964738a8141b716d635061aba1f
Hagai <hagaihamami@gmail.com>
2026-06-27 21:00:49 +0300

    organize textarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 
+import AddNotes from "./components/AddNotes"
+
 function App(){
 
   return (
     <>
-      <h1>Quick Notes</h1>
+      <h1>Quick Notes2</h1>
+      {/* input + add button */}
+      <AddNotes />
+      {/* search bar */}
+
+      {/* saved notes */}
     </>
   )
 }
---
+++ src/components/AddNotes.jsx
@@ -0,0 +1,10 @@
+function AddNotes(){
+    return (
+        <div className="input-panel">
+            <textarea placeholder="Write your text here..."></textarea>
+            <button>Add</button>
+        </div>
+    )
+}
+
+export default AddNotes
